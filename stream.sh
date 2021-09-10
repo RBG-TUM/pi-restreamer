71cf460bb6cfd65307fd3c5693399eab57b60bb5
Joscha Henningsen <joscha.henningsen@tum.de>
2021-09-10 20:30:21 +0200

    update usb device name

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 # TODO
 
 # update the video device for grep to fit your needs:
-ffmpeg -re -i "$(v4l2-ctl --list-devices | grep -A 1 Logitech | tail -n 1 | tr -d '[[:blank:]]')" \
+ffmpeg -re -i "$(v4l2-ctl --list-devices | grep -A 1 Magewell | tail -n 1 | tr -d '[[:blank:]]')" \
     -framerate 30 -video_size 1920x1080 -c:v libx264 -preset veryfast -tune zerolatency \
     -b:v 1500k -maxrate 3000k -bufsize 3000k -vf "scale=1920:-1,format=yuv420p" -g 60 \
     -c:a aac -b:a 128k -ar 44100 \
